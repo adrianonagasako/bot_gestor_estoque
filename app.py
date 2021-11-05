b604406a71beb5f5d905d8975816483c9e7843bb
adrianonagasako <adrianotamio@hotmail.com>
2021-11-05 17:48:20 -0300

    start0511

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-from telethon import TelegramClient, events, sync
+from telethon import TelegramClient, events
 from decouple import config
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
-api_id = config('API_ID')
-api_hash = config('API_HASH')
 
-client = TelegramClient('session_name', api_id, api_hash)
-client.start()
+def connection():
+    api_id = config('API_ID')
+    api_hash = config('API_HASH')
 
-print(client.get_me().stringify())
+    client = TelegramClient('session_name', api_id, api_hash)
+    return client
 
-client.send_message('username', 'Hello! Talking to you from Telethon')
-client.send_file('username', '/home/myself/Pictures/holidays.jpg')
 
-client.download_profile_photo('me')
-messages = client.get_messages('username')
-messages[0].download_media()
+def main():
+    while True:
+        @client.on(events.NewMessage())
+        async def handler(event):
+            chat = await event.get_chat()
+            sender = await event.get_sender
+            with open('F:\workspaces\bot_gestor_estoque\log.txt', 'w') as l:
+                l.write(f"Chat: {chat}\n Sender: {sender}\n")
+            print(f'Chat: {chat}')
+            print(f'Sender: {sender}')
+            await event.reply('Hey!')
 
-@client.on(events.NewMessage(pattern='(?i)hi|hello'))
-async def handler(event):
-    await event.respond('Hey!')
+#print(client.get_me().stringify())
+#
+#client.send_message('username', 'Hello! Talking to you from Telethon')
+#client.send_file('username', '/home/myself/Pictures/holidays.jpg')
+#
+#client.download_profile_photo('me')
+#messages = client.get_messages('username')
+#messages[0].download_media()
+
+
+if __name__ == '__main__':
+    conn = connection()
+    client = conn.start()
+    client.run_until_disconnected(main())
